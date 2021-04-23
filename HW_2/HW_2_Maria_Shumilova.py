16747ebb84716c84e5664d76e1df3727870b0b07
Maria Shumilova <78758137+mmshumilova@users.noreply.github.com>
2021-04-23 18:33:25 +0300

    Add files via upload

---
+++ HW_2/HW_2_Maria_Shumilova.py
@@ -0,0 +1,32 @@
+our_str = 'ATAGCCGA' # our string
+x = 0.6   # CG-content
+N = 90000 # amount of random DNA strings
+
+
+p_our_str = 1
+p_letter_AT = (1-x)/2
+p_letter_CG = x/2
+
+## 1) prob to get our string
+
+for letter in our_str:
+    if letter == 'G':
+        p_our_str *= p_letter_CG
+    elif letter == 'C':
+        p_our_str *= p_letter_CG
+    elif letter == 'A':
+        p_our_str *= p_letter_AT
+    elif letter == 'T':
+        p_our_str *= p_letter_AT
+
+print(p_our_str)
+
+# 2) the prob don't get our string in N random DNA strings
+
+ 
+p_not_our_str = (1-p_our_str)**N # N - amount of random DNA strings
+print(p_not_our_str)
+
+# 3) what's the pbob to get at least 1 our string in N random DNA string
+
+print(1 - p_not_our_str)
